0c77196405f3a7a5db80f44ff1a75e0048337888
AlersonYampi <alerson@bubb.com.br>
2019-04-26 14:51:41 +0000

    Apply fixes from StyleCI

--- src/Yampi/Anymarket/Contracts/CallbackInterface.php
+++ src/Yampi/Anymarket/Contracts/CallbackInterface.php
@@ -5,8 +5,12 @@ namespace Yampi\Anymarket\Contracts;
 interface CallbackInterface
 {
     public function get($offset = 0, $limit = 50);
+
     public function create(array $params);
+
     public function update($id, array $params);
+
     public function find($id);
+
     public function delete($id);
 }
